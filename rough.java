bea475d25b07dadd71b5a3184d8e4dff571d990a
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-09-26 21:04:58 +0530

    Leetcode

--- rough.java
+++ rough.java
@@ -31,24 +31,46 @@
 
 
 
-import java.util.*;
+// import java.util.*;
+// class Solution {
+//     public int[] intersect(int[] nums1, int[] nums2) {
+//         List<Integer> list = new ArrayList<>();
+//         int idx = 0;
+//         for(int i=0;i<nums1.length;i++){
+//             while(idx < nums2.length){
+//                 if(nums1[i] == nums2[idx]){
+//                     list.add(nums2[idx]);
+//                     break;
+//                 }
+//                 idx++;
+//             }
+//         }
+//         int[] arr = new int[list.size()];
+//         for(int i=0;i<list.size();i++){
+//             arr[i] = list.get(i);
+//         }
+//         return arr;
+//     }
+// }
+
+
+
 class Solution {
-    public int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> list = new ArrayList<>();
-        int idx = 0;
-        for(int i=0;i<nums1.length;i++){
-            while(idx < nums2.length){
-                if(nums1[i] == nums2[idx]){
-                    list.add(nums2[idx]);
-                    break;
-                }
-                idx++;
+    public int smallestRangeII(int[] nums, int k) {
+        Arrays.sort(nums);
+        int max = nums[nums.length - 1];
+        for(int i=0;i<nums.length;i++){
+            if(i == nums.length-1){
+                nums[i] = nums[i] - k;
+                break;
+            }
+            if(nums[i] <= max){
+                nums[i] = nums[i] + k;
             }
         }
-        int[] arr = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            arr[i] = list.get(i);
-        }
-        return arr;
+        Arrays.sort(nums);
+        int maxF = nums[nums.length-1];
+        int minF = nums[0];
+        return maxF - minF;
     }
 }
